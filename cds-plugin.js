c7132ab1b8728e159be76ec72bdee1ee62bccef9
Richard Lindner <rlindner81@gmail.com>
2023-09-01 08:20:20 +0200

    wip 8.6

--- cds-plugin.js
+++ cds-plugin.js
@@ -4,13 +4,15 @@
 const cds = require("@sap/cds");
 const { initializeFeatures } = require("./src/singleton");
 
+const activate = async () => {
+  if (cds.env.featureToggles && cds.env.featureToggles.plugin) {
+    await initializeFeatures({
+      config: cds.env.featureToggles.config,
+      configFile: cds.env.featureToggles.configFile,
+    });
+  }
+};
+
 module.exports = {
-  activate: async () => {
-    if (cds.env.featureToggles && cds.env.featureToggles.plugin) {
-      await initializeFeatures({
-        config: cds.env.featureToggles.config,
-        configFile: cds.env.featureToggles.configFile,
-      });
-    }
-  },
+  activate,
 };
