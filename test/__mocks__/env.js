80d74bad9b5f37ee12d889a40d9241a50f68a85a
Richard Lindner <rlindner81@gmail.com>
2023-11-29 11:20:16 +0100

    start with tests

--- test/__mocks__/env.js
+++ test/__mocks__/env.js
@@ -1,15 +1,13 @@
 "use strict";
 
-let cfEnv = {};
-const _reset = () => {
-  cfEnv.isOnCf = false;
-  cfEnv.cfApp = {};
-  cfEnv.cfServices = {};
-  cfEnv.cfServiceCredentialsForLabel = jest.fn().mockReturnValue({});
+const cfEnv = {
+  _reset() {
+    this.isOnCf = false;
+    this.cfApp = {};
+    this.cfServices = {};
+    this.cfServiceCredentialsForLabel = jest.fn().mockReturnValue({});
+  },
 };
-_reset();
+cfEnv._reset();
 
-module.exports = {
-  cfEnv,
-  _reset,
-};
+module.exports = cfEnv;
