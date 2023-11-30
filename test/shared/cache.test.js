1bf9233133742c533c107f71ad49bb1f82301f56
Richard Lindner <rlindner81@gmail.com>
2023-11-30 13:42:11 +0100

    remove unused imports

--- test/shared/cache.test.js
+++ test/shared/cache.test.js
@@ -1,5 +1,5 @@
 "use strict";
-const { LazyCache, ExpiringLazyCache, LimitedLazyCache, DEFAULT_EXPIRATION_GAP } = require("../../src/shared/cache");
+const { LazyCache, LimitedLazyCache } = require("../../src/shared/cache");
 
 let cache;
 
