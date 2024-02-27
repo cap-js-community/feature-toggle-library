40c62b2d154c3d729948e6757399e0c4307ae8ad
Richard Lindner <rlindner81@gmail.com>
2024-02-27 07:41:27 +0100

    start of semaphore.test.js

---
+++ test/shared/semaphore.test.js
@@ -0,0 +1,12 @@
+"use strict";
+const { Semaphore } = require("../../src/shared/semaphore");
+
+describe("semaphore", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("make exclusive", async () => {
+    // TODO code on
+  });
+});
