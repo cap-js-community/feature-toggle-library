9e3fb12d4da5ede1c1d36d13a54107a943ebf91d
Richard Lindner <rlindner81@gmail.com>
2024-03-12 09:13:42 +0100

    add semaphore test

--- test/shared/semaphore.test.js
+++ test/shared/semaphore.test.js
@@ -1,12 +1,62 @@
 "use strict";
+const { promisify, format } = require("util");
+
 const { Semaphore } = require("../../src/shared/semaphore");
 
+const sleep = promisify(setTimeout);
+
 describe("semaphore", () => {
+  let executionLog;
+  const n = 3;
+  const result = "finished";
+  const runner = async (index) => {
+    executionLog.push(format("started %d", index));
+    await sleep(10);
+    executionLog.push(format("finished %d", index));
+    return result;
+  };
+
   beforeEach(() => {
-    jest.clearAllMocks();
+    executionLog = [];
   });
 
-  test("make exclusive", async () => {
-    // TODO code on
+  test("make exclusive queueing", async () => {
+    const exclusiveRunner = Semaphore.makeExclusiveQueuing(runner);
+    const results = await Promise.all(Array.from({ length: n }, (_, i) => exclusiveRunner(i)));
+    expect(results).toMatchInlineSnapshot(`
+      [
+        "finished",
+        "finished",
+        "finished",
+      ]
+    `);
+    expect(executionLog).toMatchInlineSnapshot(`
+      [
+        "started 0",
+        "finished 0",
+        "started 1",
+        "finished 1",
+        "started 2",
+        "finished 2",
+      ]
+    `);
+  });
+
+  test("make exclusive returning", async () => {
+    const exclusiveRunner = Semaphore.makeExclusiveReturning(runner);
+    const results = await Promise.all(Array.from({ length: n }, (_, i) => exclusiveRunner(i)));
+    expect(results).toMatchInlineSnapshot(`
+      [
+        "finished",
+        undefined,
+        undefined,
+      ]
+    `);
+    expect(executionLog).toMatchInlineSnapshot(`
+      [
+        "started 0",
+        "finished 0",
+      ]
+    `);
   });
 });
