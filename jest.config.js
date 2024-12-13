dc4c0e45b4a1cf79ade404aab75945a11da4eedf
Richard Lindner <rlindner81@gmail.com>
2024-12-13 13:17:57 +0100

    leave coverage reporters empty by default to just capture summary

--- jest.config.js
+++ jest.config.js
@@ -36,12 +36,7 @@ const config = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: [
-    // "json",
-    // "text",
-    "lcov",
-    // "clover",
-  ],
+  coverageReporters: [],
 
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
