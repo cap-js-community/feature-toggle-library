8cbedb99c956f77b7fe9dd3bf0b45d4c8c281855
Richard Lindner <rlindner81@gmail.com>
2025-06-10 13:21:02 +0200

    wip 102

--- test-cap-server/cds-test-check-service.test.js
+++ test-cap-server/cds-test-check-service.test.js
@@ -3,10 +3,6 @@
 const cds = require("@sap/cds");
 const toggles = require("../src");
 
-// TODO how do we get our plugin and @sap/cds-mtxs loaded as part of the plugin startup here
-// NOTE: the cds.test server naturally loads the plugins of the passed in directory test-cap-server, which
-//   in turn defaults to the plugins of the project root, i.e., our cap dev dependencies. Unfortunately, this does
-//   not include our own plugin. So, we patch it in here...
 const server = cds.test("test-cap-server");
 const systemCall = { validateStatus: () => true, auth: { username: "system", password: "system" } };
 
@@ -23,7 +19,6 @@ describe("test-cap-server check-service", () => {
   });
 
   test("priority endpoint with feature is true", async () => {
-    const bla = cds.env;
     await toggles.changeFeatureValue("/fts/check-service-extension", true);
     expect(toggles.getFeatureValue("/fts/check-service-extension")).toBe(true);
     const response = await server.get("/rest/check/priority", systemCall);
