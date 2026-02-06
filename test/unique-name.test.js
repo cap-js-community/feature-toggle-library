dd577ea8cf341eeed6730a43ff29602fecec2542
Richard Lindner <rlindner81@gmail.com>
2026-02-06 10:37:07 +0100

    Fb/remove blue green suffixes (#134)
    * ignore idle or live suffix
    
    * add test for new behavior

--- test/unique-name.test.js
+++ test/unique-name.test.js
@@ -17,6 +17,26 @@ describe("singleton", () => {
     Reflect.deleteProperty(process.env, "VCAP_APPLICATION");
   });
 
+  test("singleton unique name can be set via cf app name, but ignores blue green suffix -idle", async () => {
+    process.env.VCAP_APPLICATION = JSON.stringify({ application_name: "test_app_name-idle" });
+
+    const toggles = require("../src/");
+    const FeatureToggles = toggles.constructor;
+    expect(FeatureToggles._getDefaultUniqueName()).toMatchInlineSnapshot(`"test_app_name"`);
+
+    Reflect.deleteProperty(process.env, "VCAP_APPLICATION");
+  });
+
+  test("singleton unique name can be set via cf app name, but ignores blue green suffix -live", async () => {
+    process.env.VCAP_APPLICATION = JSON.stringify({ application_name: "test_app_name-live" });
+
+    const toggles = require("../src/");
+    const FeatureToggles = toggles.constructor;
+    expect(FeatureToggles._getDefaultUniqueName()).toMatchInlineSnapshot(`"test_app_name"`);
+
+    Reflect.deleteProperty(process.env, "VCAP_APPLICATION");
+  });
+
   test("singleton unique name can be set via env and beats cf app name", async () => {
     process.env.VCAP_APPLICATION = JSON.stringify({ application_name: "test_app_name" });
     process.env[ENV.UNIQUE_NAME] = "test_unique_name";
